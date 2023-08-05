d94a6f3f035b2850210b73bdd25a8cf605473cec
bravindan <danielbravin0@gmail.com>
2023-08-05 17:54:12 +0300

    seconf commit, added the game logic

--- Game.js
+++ Game.js
@@ -5,23 +5,35 @@ import {
   ScrollView,
   TextInput,
   TouchableOpacity,
+  Alert,
+
 } from "react-native";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function Game() {
-  const [num, setNum] = useState(0);
+  const [guess, setGuess] = useState(0);
+  const [attempts, setAttempts] = useState(0)
+  const maxAttempts =3
+  const min = 1;
+  const max = 100;
+  const randomInRange = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  const inputHandler =(event)=>{
-      setNum(event.target.value);
+
+  const matchNumber = () =>{  
+    const guessed = parseInt(guess)
+    setAttempts(attempts + 1);
+    if(guessed<1 || guessed>=100) {
+      Alert.alert('Warning','Number must be between 0 and 100',[])
+    }else if(guessed===randomInRange){
+      Alert.alert('Congrats','You got it right')
+      // setGuess(0)
+    }else if (attempts === maxAttempts) {
+      Alert.alert(`GAME OVER!`, `You've used all your attempts. The correct number was ${randomInRange}.`);
+    } else {
+      Alert.alert('Try again!', 'You have missed it.');
+    }
+
   }
-
-  const matchNumber = () =>{
-    console.log(num)
-  }
-
-  useEffect(() => {
-      matchNumber();
-    }, [num]);
 
   return (
     <ScrollView>
@@ -30,11 +42,11 @@ export default function Game() {
       </View>
       <View style={styles.inputContainer}>
         <TextInput
-          value={num}
-          onChange={inputHandler}
+          value={guess}
+          onChangeText={(value) =>setGuess(value)}
           style={styles.input}
           keyboardType="numeric"
-          maxLength={3}
+          maxLength={2}
           placeholder="Enter a number between 1 to 100"
         />
       </View>
